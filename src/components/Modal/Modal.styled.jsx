fc64667c4e618ff253161a436fb98b6fc31549e5
VadimKorobec <_s_e_t__@ukr.net>
2023-05-15 17:19:07 +0300

    Create Modal.styled.jsx

---
+++ src/components/Modal/Modal.styled.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  min-height: 300px;
+  max-width: 600px;
+  width: 100%;
+  padding: 12px;
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+    rgba(0, 0, 0, 0.22) 0px 15px 12px;
+`;
