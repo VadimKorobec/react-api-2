4a1fa68f5542f808a898b5c8abc873f76dc883cc
VadimKorobec <_s_e_t__@ukr.net>
2023-05-16 17:06:33 +0300

    Update ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import { ImageCalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <ImageCalleryItem />
+        </li>
+      </ul>
+    </div>
+  );
+};
